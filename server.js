0f40c09925d25ee3dcfd991a23dda7fceeb26fdd
Kadeem Best <kadeem.best@theweekendhack.com>
2021-11-08 03:16:35 -0500

    Adjusted Cors

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const app = express();
 
 const corsOptionsDelegate = function (req, callback) 
 {
-  const allowlist = [`http://localhost:3000`, 'http://127.0.0.1:3000','https://gracious-nobel-f10603.netlify.app']
+  const allowlist = [`http://localhost:3000`, 'http://127.0.0.1:3000','https://practical-mirzakhani-548a06.netlify.app']
   let corsOptions;
   if (allowlist.indexOf(req.header('Origin')) !== -1) {
     corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
